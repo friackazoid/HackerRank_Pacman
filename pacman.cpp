9757cff03f656fd18b92bdfab256a7dcd2c9a43e
Alina Stepina <stepina@arrival.com>
2023-03-13 11:00:10 +0100

    ADD: add astar in common solution

--- pacman.cpp
+++ pacman.cpp
@@ -284,23 +284,26 @@ void pacman_ucs_solve ( int r, int c, int pacman_r, int pacman_c, int food_r, in
         std::cout << r_it->first  << " " << r_it->second << std::endl;
 }
 
+// Here, we made a small hack by utilizing the fact that the robot's 
+// movement is restricted to left, right, up, and down directions. 
+// Instead of using the Manhattan distance as the g_score, we extended the g_score value at each step
 void pacman_astar_solve ( int r, int c, int pacman_r, int pacman_c, int food_r, int food_c, std::vector<std::string> const& grid) {
     std::vector<pacman_state_t> result_path; 
     std::vector<pacman_state_t> explored_node; 
 
-    struct UCSHeuristic {
+    struct AStarHeuristic {
         int food_r_, food_c_;
         int operator() (pacman_state_t const& s) { return (s.first == food_r_ && s.second == food_c_) ? 1 : 0; }
-    } ;
+    };
 
-    struct UCSComparator{
+    struct AStarComparator{
         bool operator() ( pacman_node_t const l, pacman_node_t const r ) { return l->get_total_score() < r->get_total_score(); }
     };
 
     a_star_search::NodeVisitor<pacman_state_t,
         PacmanNeighborFunctor, PacmanStateFilter,
-        std::priority_queue<pacman_node_t, std::vector<pacman_node_t>, UCSComparator>,
-        UCSHeuristic> pacman_node_visitor(PacmanStateFilter{r, c, grid}, UCSHeuristic{food_r, food_c});
+        std::priority_queue<pacman_node_t, std::vector<pacman_node_t>, AStarComparator>,
+        AStarHeuristic> pacman_node_visitor(PacmanStateFilter{r, c, grid}, AStarHeuristic{food_r, food_c});
 
     a_star_search::a_star<pacman_state_t> ( 
             {pacman_r, pacman_c},
@@ -339,7 +342,8 @@ int main(void) {
 
 //    read_data<decltype(pacman_task::pacman_dfs_solve)> (pacman_task::pacman_dfs_solve);
 //    read_data<decltype(pacman_task::pacman_bfs_solve)> (pacman_task::pacman_bfs_solve);
-      read_data<decltype(pacman_task::pacman_ucs_solve)> (pacman_task::pacman_ucs_solve);    
+//    read_data<decltype(pacman_task::pacman_ucs_solve)> (pacman_task::pacman_ucs_solve);    
+      read_data<decltype(pacman_task::pacman_astar_solve)> (pacman_task::pacman_astar_solve);    
 
     return 0;
 }
